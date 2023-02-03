9a06e1210d510355790623dce63b4c2985822269
EssaadaniYounes <younesessaadani2003@gmail.com>
2023-02-03 12:41:10 +0100

    "ex3 done"

--- src/ex3/Ex3.jsx
+++ src/ex3/Ex3.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Ex3() {
+  const [countries, setCountries] = useState([]);
+  //Q1 Code qui recupere les donnees dans le variable countries
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/countries")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  // Q2 Code D'afficher les pays avec nombre de cases
   return (
-    <div>Tomorrow nchaalh</div>
-  )
+    <ul>
+      {countries.map((c) => (
+        <li key={c.country}>
+          {c.country}------{c.cases}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default Ex3
+export default Ex3;
